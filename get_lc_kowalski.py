ff247f8e25705da3c7aac207cb58043e23df35ac
igorandreoni <igor.andreoni@gmail.com>
2020-02-03 19:15:00 -0800

    Added more keys and prv_candidates

--- get_lc_kowalski.py
+++ get_lc_kowalski.py
@@ -1,7 +1,48 @@
 from astropy.io import ascii
-from astropy.table import Table
+from astropy.table import Table, unique
+import numpy as np
+import pdb
 
 from penquins import Kowalski
+
+
+def get_lightcurve_alerts_aux(username, password, list_names):
+    """Query the light curve for a list of candidates"""
+
+    k = Kowalski(username=username, password=password, verbose=False)
+    q = {"query_type": "find",
+         "query": {
+                   "catalog": "ZTF_alerts_aux",
+                   "filter": {
+                              '_id': {'$in': list(list_names)}
+                              },
+                   "projection": {}
+                       },
+         "kwargs": {"hint": "_id_"}
+         }
+
+    '''
+    q = {"query_type": "info",
+         "query": {
+             "command": "index_info",
+             "catalog": "ZTF_alerts"
+         }
+         }
+    # ZTF19acdpipl
+    '''
+    r = k.query(query=q)
+    #indexes = r['result_data']['query_result']
+    #for ii, (kk, vv) in enumerate(indexes.items()):
+    #    print(f'index #{ii+1}: "{kk}"\n{vv["key"]}\n')
+    if r['result_data']['query_result'] == []:
+        print("No candidates to be checked?")
+        return None
+    out = []
+    for l in r['result_data']['query_result']:
+        with_det = list({'objectId': l['_id'], 'candidate': s} for s in l['prv_candidates'] if 'magpsf' in s.keys())
+        out = out + with_det
+
+    return out
 
 
 def get_lightcurve_alerts(username, password, list_names):
@@ -24,7 +65,16 @@ def get_lightcurve_alerts(username, password, list_names):
                                   "candidate.sigmapsf": 1,
                                   "candidate.programid": 1,
                                   "candidate.magzpsci": 1,
-                                  "candidate.magzpsciunc": 1
+                                  "candidate.magzpsciunc": 1,
+                                  "candidate.sgscore1": 1,
+                                  "candidate.sgscore2": 1,
+                                  "candidate.sgscore3": 1,
+                                  "candidate.distpsnr1": 1,
+                                  "candidate.distpsnr2": 1,
+                                  "candidate.distpsnr3": 1,
+                                  "candidate.field": 1,
+                                  "candidate.rcid": 1,
+                                  "candidate.pid": 1
                                   }
                        },
          "kwargs": {"hint": "objectId_1"}
@@ -45,22 +95,41 @@ def create_tbl_lc(light_curves, outfile):
     # fid -> filter
     filters = {'1': 'g', '2': 'r', '3': 'i'}
 
-    tbl = Table([[], [], [], [], [], [], [], [], [], []],
+    tbl = Table([[], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
+                 [], [], [], []],
                 names=('name', 'ra', 'dec', 'jd', 'magpsf', 'sigmapsf',
                        'filter', 'magzpsci', 'magzpsciunc',
-                       'programid'),
+                       'programid', 'field', 'rcid', 'pid',
+                       'sgscore1', 'sgscore2', 'sgscore3',
+                       'distpsnr1', 'distpsnr2', 'distpsnr3'),
                 dtype=('S12', 'double', 'double', 'double',
-                       'f', 'f', 'S', 'f', 'f', 'i'))
+                       'f', 'f', 'S', 'f', 'f', 'i', 'i', 'i', 'int_',
+                       'f', 'f', 'f', 'f', 'f', 'f'))
 
     for l in light_curves:
         magzpsci = l["candidate"].get("magzpsci")
         magzpsciunc = l["candidate"].get("magzpsciunc")
-        row = [l["objectId"], l["candidate"]["ra"], l["candidate"]["dec"],
+        try:
+            row = [l["objectId"], l["candidate"]["ra"], l["candidate"]["dec"],
                l["candidate"]["jd"], l["candidate"]["magpsf"],
                l["candidate"]["sigmapsf"], filters[str(l["candidate"]["fid"])],
                magzpsci, magzpsciunc,
-               l["candidate"]["programid"]]
+               l["candidate"]["programid"], l["candidate"]["field"],
+               l["candidate"]["rcid"], l["candidate"]["pid"], 
+               l["candidate"]["sgscore1"], l["candidate"]["sgscore2"],
+               l["candidate"]["sgscore3"], l["candidate"]["distpsnr1"],
+               l["candidate"]["distpsnr2"], l["candidate"]["distpsnr3"]]
+        except KeyError:
+            row = [l["objectId"], l["candidate"]["ra"], l["candidate"]["dec"],
+               l["candidate"]["jd"], l["candidate"]["magpsf"],
+               l["candidate"]["sigmapsf"], filters[str(l["candidate"]["fid"])],
+               magzpsci, magzpsciunc,
+               l["candidate"]["programid"], l["candidate"]["field"],  
+               l["candidate"]["rcid"], l["candidate"]["pid"], np.nan,
+               np.nan, np.nan, np.nan, np.nan, np.nan]
         tbl.add_row(row)
+    # Remove exact duplicates
+    tbl = unique(tbl)
     tbl.sort("jd")
     tbl.write(outfile, format='csv', overwrite=True)
 
@@ -83,12 +152,19 @@ the coordinates will be queried from kowalski', default=None)
         exit()
 
     # Read the secrets
-    secrets = ascii.read('secrets.csv', format='csv')
+    secrets = ascii.read('../kowalski/secrets.csv', format='csv')
     username_kowalski = secrets['kowalski_user'][0]
     password_kowalski = secrets['kowalski_pwd'][0]
 
     # Get the light curves
-    light_curves = get_lightcurve_alerts(username_kowalski, password_kowalski,
+    light_curves_alerts = get_lightcurve_alerts(username_kowalski, password_kowalski,
                                          args.names)
+    
+    # Add prv_candidates photometry to the light curve
+    light_curves_aux = get_lightcurve_alerts_aux(username_kowalski, password_kowalski,
+                                         args.names)
+
+    light_curves = light_curves_alerts + light_curves_aux
+
     # Create a table and output CSV file
     create_tbl_lc(light_curves, args.out)
